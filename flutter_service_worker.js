fbece34ccfff5e48eda5b455aa39f6c575ca4966
Monika <monika.nb@indiahealthlink.com>
2024-09-11 18:46:29 +0530

    social media push

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "04e1ee362c5f48514e78642d59ee1bdf",
+"flutter_bootstrap.js": "acee1871c5255c17f70215509364980b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "3f8a82e51186b06d994e22feb50cd8a1",
-"/": "3f8a82e51186b06d994e22feb50cd8a1",
-"main.dart.js": "be0674ddafd204e01edc93f90bc9dc45",
+"index.html": "c3580a22ad95343aa165106fd9f2bcaf",
+"/": "c3580a22ad95343aa165106fd9f2bcaf",
+"main.dart.js": "aa472f435d53480f8703ca740f46d699",
 "manifest.json": "f7533172d1fc906e47c2798555446b27",
 "version.json": "8aaff6cf268b8aeff4f093f6e989fb3a"};
 // The application shell files that are downloaded before a service worker can
